aff207b268ea3784771346688f41c2da9940916f
mayoworks <64965382+bberryw31@users.noreply.github.com>
2024-03-17 22:21:10 +0900

    Update 1032.py

--- 1032.py
+++ 1032.py
@@ -1,8 +1,31 @@
-N = input()
-for i in range(int(N)):
-    
-    i = []
-    i.extend(input())
-for j in range(int(N)):
-    for i in range(int(N)):
-        if i
+a = []
+b = []
+c = []
+
+def compare(a, b):
+    for k in range(len(a)):
+        if a[k] == b[k]:
+            c[k] = a[k]
+        else:
+            c[k] = '?'
+
+N = int(input())
+
+if N == 1:
+    print(input())
+
+else:
+    for j in range(N):
+        if len(a) == 0:
+            a.extend(input())
+        elif len(a) > 0 and len(b) == 0:
+            b.extend(input())
+            c = [] + a
+            compare(a, b)
+        else:
+            a = []
+            a.extend(input())
+            compare(a, c)
+
+    result = ''.join(c)
+    print(result)
