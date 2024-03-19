58d0e430901eae45b22968c9756504ad85eed9bf
mayoworks <64965382+bberryw31@users.noreply.github.com>
2024-03-19 16:30:32 +0900

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,28 @@
+import random
+
+def selectionsort(list):
+    for i in range(len(list)):
+        min = list[i]
+        for j in range(i, len(list)):
+            if list[j] <= min:
+                min = list[j]
+                minorder = j
+        list[i], list[minorder] = list[minorder], list[i]
+
+def randomlist(N):
+    for k in range(N):
+        list.append(random.randint(1,999))
+
+list=[]
+N = int(input())
+randomlist(N)
+
+list2 = [] + list
+selectionsort(list)
+list2.sort()
+
+print(list)
+print(list2)
+
+print(list2==list)
+
