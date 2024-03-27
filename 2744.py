27081637727350f06e9e3f1e6ed35151d1f22838
mayoworks <64965382+bberryw31@users.noreply.github.com>
2024-03-27 23:16:22 +0900

    Add files via upload

---
+++ 2744.py
@@ -0,0 +1,15 @@
+import sys
+
+word = sys.stdin.readline().strip()
+
+print(word.swapcase())
+
+# word2 = ""
+# for chrs in word:
+#     print(type(chrs))
+#     if chrs.islower():
+#         word2 += chrs.upper()
+#     else:
+#         word2 += chrs.lower()
+# print(word2)
+
