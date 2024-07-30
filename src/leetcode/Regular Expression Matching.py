33ed1bdfbba6559c6077fed94c2d21554a018468
bberryw31 <bberryw31@gmail.com>
2024-07-30 12:57:57 -0700

    JIMMY

--- src/leetcode/Regular Expression Matching.py
+++ src/leetcode/Regular Expression Matching.py
@@ -1,52 +1,16 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        prev = ""
-        temp = ""
-        while s and p:
-            if p[0] == ".":
-                p = p[1:]
-                s = s[1:]
-                prev = "."
-                print (s,p)
-            elif p[0] == "*":
-                if prev == ".":
-                    s = ""
-                    p = p[1:]
-                    print (s,p)
-                else:
-                    while s and s[0] == prev:
-                        s = s[1:]
-                        if len(p)>1 and p[1] == prev:
-                            p = p[0] + p[2:]
-                    p = p[1:]
-                    print (s,p)
+        temp = []
+        for p1 in p:
+            if p1 != "*":
+                temp.append(p1)
             else:
-                if p[0] == s[0]:
-                    prev = p[0]
-                    p = p[1:]
+                temp[-1]+="*"
+        for t in temp:
+            if len(t)<2:
+                if t == s[0]:
                     s = s[1:]
-                    print (s,p)
-                elif len(p)>1 and p[1] == "*":
-                    p = p[2:]
-                    prev = "*"
-                    print (s,p)
                 else:
                     return False
                     exit
-        while p:
-            if p[0] == "*":
-                p = p[1:]
-                print (s,p)
-            elif len(p)>1 and p[1] == "*":
-                p = p[2:]
-                print (s,p)
             else:
-                return False
-                exit
-        if s:
-            return False
-        else:
-            return True
-
-
-print(Solution().isMatch("aaa","ab*a*c*a"))
